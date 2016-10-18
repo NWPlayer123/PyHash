664194d39ad384eb0cb6f4edd074407cdd4649af
NWPlayer123 <artemisialmoon@gmail.com>
2016-10-17 18:54:47 -0600

    added the script

---
+++ hash.py
@@ -0,0 +1,25 @@
+from sys import argv
+from hashlib import md5
+from tkinter import Tk, Entry
+
+try:
+    with open(argv[1], "rb") as f:
+        hashed = md5(f.read())
+        hashed = hashed.hexdigest().upper()
+        root = Tk()
+        root.title("Hash")
+        root.resizable(0, 0)
+        e = Entry(root, width=32)
+        e.pack()
+        e.insert(0, hashed)
+        e.configure(state="readonly")
+        root.mainloop()
+except:
+        root = Tk()
+        root.title("Hash")
+        root.resizable(0, 0)
+        e = Entry(root, width=32)
+        e.pack()
+        e.insert(0, "No input file")
+        e.configure(state="readonly")
+        root.mainloop()
